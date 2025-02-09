d96d22b48d66041fd60d4c7d1f209fa4e64296aa
sjtroxel <sjtroxel@alumni.iu.edu>
2025-02-09 17:03:15 -0600

    delete files from HTML when clicking delete

--- SSD-Tutorial-14-(Modules)/scripts/checkout.js
+++ SSD-Tutorial-14-(Modules)/scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
 
    
 cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -103,5 +103,7 @@ document.querySelectorAll('.js-delete-link')
             const productId = link.dataset.productId;
             removeFromCart(productId);
             
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         });
     });
